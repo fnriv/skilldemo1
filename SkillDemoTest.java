910fe515bad241c462071ed68f1589afd16fdae1
fnriv <fnrivera@ucsd.edu>
2022-05-01 16:12:39 -0700

    replace junit files

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SkillDemoTest {
+    
     @Test
     public void last3Test() {
         assertEquals("triton", SkillDemo.last3("triton")); //failing test
